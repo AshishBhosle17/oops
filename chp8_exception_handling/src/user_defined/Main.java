ef6f21aebe1e0ba99e10fa1a1816d9c42770fef3
Ashish Bhosle <asbhosle2003@gmail.com>
2025-02-28 00:04:34 +0530

    push

--- chp8_exception_handling/src/user_defined/Main.java
+++ chp8_exception_handling/src/user_defined/Main.java
@@ -1,5 +1,31 @@
 package user_defined;
 
+class Emp {
+	int id;
+	String name;
+	double sal;
+	String add;
+	
+	public Emp(int id, String name, double sal) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+	}
+	
+	void meth() {
+		System.out.println(name);
+		System.out.println(id);
+		System.out.println(sal);
+	}
+	
+}
 public class Main {
+	public static void main(String[] args) {
+		
+		Emp emp = new Emp(9, "Ashish", 20000);
+		emp.meth();
+		System.out.println(emp.add);
+	}
 
 }
