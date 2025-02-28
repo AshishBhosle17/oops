3460ed99ceecf2253cbd9261bf6d9d5938f0eb4b
Ashish Bhosle <asbhosle2003@gmail.com>
2025-03-01 00:02:10 +0530

    vectors ops and obj creation

---
+++ chp12_collections/src/vector/Main.java
@@ -0,0 +1,17 @@
+package vector;
+
+import java.util.Vector;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Emp emp = new Emp(9, "Ashish", 25000);
+		
+		Emp emp1 = new Emp(1, "Jerry", 30000);
+		Emp emp2 = new Emp(2, "Harry", 30000);
+		Emp emp3 = new Emp(3, "Merry", 30000);
+		
+		Vector<Emp> emps = new Vector<Emp>();
+	}
+
+}
